8822c3c1f2db56503095a43c53378e30a36e8705
emmaoberstein <emmaoberstein@gmail.com>
2016-02-17 01:24:08 -0500

    camera action for adding to closet

--- WeatherWear/app/src/main/java/weatherwear/weatherwear/DisplayCategoryActivity.java
+++ WeatherWear/app/src/main/java/weatherwear/weatherwear/DisplayCategoryActivity.java
@@ -1,7 +1,12 @@
 package weatherwear.weatherwear;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -12,11 +17,24 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.soundcloud.android.crop.Crop;
+
+import java.io.File;
 
 /**
  * Created by Emma on 2/17/16.
  */
 public class DisplayCategoryActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_TAKE_FROM_CAMERA = 0;
+    public static final int REQUEST_CODE_SELECT_FROM_GALLERY = 1;
+
+    public static final int REQUEST_CODE_CROP_PHOTO = 2;
+    private Uri mImageCaptureUri, mTempUri;
+    private Boolean stateChnaged = false, cameraClicked = false,clickedFromCam=false;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_activity);
@@ -37,7 +55,89 @@ public class DisplayCategoryActivity extends AppCompatActivity {
         return true;
     }
 
-    public void addItem(MenuItem item){
+    public void changeImage(){
+        Intent intent;
 
+
+        // Take photo from camera，
+        // Construct an intent with action
+        // MediaStore.ACTION_IMAGE_CAPTURE
+        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Construct temporary image path and name to save the taken
+        // photo
+        mImageCaptureUri = Uri.fromFile(new File(Environment
+                .getExternalStorageDirectory(), "tmp_"
+                + String.valueOf(System.currentTimeMillis()) + ".jpg"));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                mImageCaptureUri);
+        intent.putExtra("return-data", true);
+        try {
+            // Start a camera capturing activity
+            // REQUEST_CODE_TAKE_FROM_CAMERA is an integer tag you
+            // defined to identify the activity in onActivityResult()
+            // when it returns
+            startActivityForResult(intent, REQUEST_CODE_TAKE_FROM_CAMERA);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK)
+            return;
+
+        switch (requestCode) {
+            case REQUEST_CODE_TAKE_FROM_CAMERA:
+                // Send image taken from camera for cropping
+                beginCrop(mImageCaptureUri);
+                clickedFromCam=true;
+                break;
+
+            case REQUEST_CODE_SELECT_FROM_GALLERY:
+                Uri srcUri = data.getData();
+                beginCrop(srcUri);
+                break;
+
+            case Crop.REQUEST_CROP:
+                // Update image view after image crop
+                // Set the picture image in UI
+                handleCrop(resultCode, data);
+
+                // Delete temporary image taken by camera after crop.
+                if(clickedFromCam) {
+                    File f = new File(mImageCaptureUri.getPath());
+                    if (f.exists())
+                        f.delete();
+                    clickedFromCam=false;
+                }
+
+                break;
+        }
+    }
+
+    /** Method to start Crop activity using the library
+     *	Earlier the code used to start a new intent to crop the image,
+     *	but here the library is handling the creation of an Intent, so you don't
+     * have to.
+     *  **/
+    private void beginCrop(Uri source) {
+        Uri destination = Uri.fromFile(new File(getCacheDir(), "cropped"));
+        Crop.of(source, destination).asSquare().start(this);
+    }
+
+    private void handleCrop(int resultCode, Intent result) {
+        if (resultCode == RESULT_OK) {
+            mTempUri = Crop.getOutput(result);
+            cameraClicked=true;
+
+
+        } else if (resultCode == Crop.RESULT_ERROR) {
+            Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void addItem(MenuItem item) {
+       changeImage();}
 }
