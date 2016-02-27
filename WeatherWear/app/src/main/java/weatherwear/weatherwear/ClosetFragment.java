58658e386fdf44e5db20b170e5e6ee85d7d2fb4f
emmaoberstein <emmaoberstein@gmail.com>
2016-02-27 18:36:57 -0500

    small bug fix

--- WeatherWear/app/src/main/java/weatherwear/weatherwear/ClosetFragment.java
+++ WeatherWear/app/src/main/java/weatherwear/weatherwear/ClosetFragment.java
@@ -82,7 +82,8 @@ public class ClosetFragment extends Fragment {
             public void onClick(View v) {
                 // Perform action on click
                 Intent intent = new Intent(getActivity(), DisplayCategoryActivity.class);
-                intent.putExtra("CATEGORY_TYPE","dresses");
+                intent.putExtra("CATEGORY_TYPE","Dresses");
+                intent.putExtra("SUBCATEGORY_TYPE","Dresses");
                 startActivity(intent);
             }
         });
